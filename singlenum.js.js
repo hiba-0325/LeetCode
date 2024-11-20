d78ab4358faedb76920cfbd00c8d638ad1227dc3
hiba-0325 <hibakm72@gmail.com>
2024-11-20 20:16:41 +0530

    single

---
+++ singlenum.js.js
@@ -0,0 +1,11 @@
+var singleNumber = function (nums) {
+    let arr = nums.sort((a, b) => a - b); 
+  
+    for (let i = 0; i <= nums.length - 1; i += 2) { // Iterate by 2
+      if (arr[i] !== arr[i + 1]) {
+        return arr[i];
+      }
+    }
+  
+    return arr[arr.length - 1];  
+  };
