fdbb66d164ab998fc0b95a63390914c94199c2cf
hiba-0325 <hibakm72@gmail.com>
2024-12-27 16:26:28 +0530

    linkedList test: done

--- linkedList.js
+++ linkedList.js
@@ -1,9 +1,13 @@
-const hasCycle = (head) => {
-    let fast = head;
-    while (fast && fast.next) {
-      head = head.next;
-      fast = fast.next.next;
-      if (head === fast) return true;
-    }
-    return false;
-  };
+const linkedList = (head) => {
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+  return false;
+};
+head = [1, 2];
+head2 = [1], pos = -1
+console.log(linkedList(head));
+console.log(linkedList(head2))
