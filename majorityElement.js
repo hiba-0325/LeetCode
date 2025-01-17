21ebddc5a2ee77cf95d8f15f5dd99415447434cc
hiba-0325 <hibakm72@gmail.com>
2025-01-17 10:13:51 +0530

    majority element

---
+++ majorityElement.js
@@ -0,0 +1,12 @@
+var majorityElement = function(nums) {
+    let candidate;
+    let count = 0;
+
+    for (const num of nums){
+        if(count ===0){
+            candidate=num;
+        }
+        count += (num === candidate)?1:-1
+    }
+    return candidate
+};
